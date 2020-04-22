c6b890a00bdc9f59fcfdcbe7325232a7c310233c
Eslam Fathy <eslmafathyfarghal@gmail.com>
2020-04-22 13:23:42 +0200

    adding : questions ,timer , buttonEvent and progressBar

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,19 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Eslam Fathy on 4/22/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let q : String
+    let a : String
+    
+    init (q :String , a:String){
+        self.q = q
+        self.a = a
+    }
+}
